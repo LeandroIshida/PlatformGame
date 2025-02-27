52c6e3003779541c4fb7c0a2aad0504e1eb8fc94
Leandro Ishida <leandroishida@hotmail.com>
2025-02-27 17:05:35 -0400

    Add Dialogue and bugs

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
     public Sprite profile;
-    public string speechTxt;
+    public string[] speechTxt;
     public string actorName;
 
-    public LayerMask playerLayer;
+    public string playerTag = "Player"; // Define a tag do jogador no Inspector
+    //public LayerMask playerLayer;
     public float radious;
     private DialogueControl dc;
+
+    bool onRadious;
+
 
     private void Start()
     {
@@ -22,15 +27,39 @@ public class Dialogue : MonoBehaviour
         Interact();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onRadious) 
+        {
+            dc.Speech(profile, speechTxt, actorName);
+        }
+    }
+
     public void Interact()
     {
-        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
+        /*Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
 
         if (hit != null)
         {
             dc.Speech(profile, speechTxt, actorName);
-        }
+        }*/
 
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious);
+
+        if (hit != null && hit.CompareTag(playerTag)) // Verifica se o Collider encontrado tem a Tag correta
+        {
+            //dc.Speech(profile, speechTxt, actorName);
+            onRadious = true;
+        }
+        else
+        {
+            onRadious = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, radious);
     }
 
 }
--- Assets/Scripts/DialogueControl.cs
+++ Assets/Scripts/DialogueControl.cs
@@ -13,14 +13,45 @@ public class DialogueControl : MonoBehaviour
 
     [Header("Settings")]
     public float typingSpeed;
+    private string[] sentences;
+    private int index;
 
-    public void Speech(Sprite p, string txt, string actorName)
+    public void Speech(Sprite p, string[] txt, string actorName)
     {
         dialogue.SetActive(true);
         profile.sprite = p;
-        speechText.text = txt;
+        sentences = txt;
         actorNameText.text = actorName;
+        StartCoroutine(TypeSentence());
 
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if(speechText.text == sentences[index])
+        {
+            if(index < sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else
+            {
+                speechText.text = "";
+                index = 0;
+                dialogue.SetActive(false);
+            }
+        }
     }
 
 }
