71ac294a66da5afdc907754d5ec9fa58f96b5deb
Leandro Ishida <leandroishida@hotmail.com>
2025-03-03 17:22:43 -0400

    Fazendo tela infinita

---
+++ Assets/Scripts/ScreenWrap.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ScreenWrap : MonoBehaviour
+{
+    private Rigidbody2D myRigiBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigiBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        float rightSideOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x;
+        float leftSideOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(0f, 0f)).x;
+
+        float topSideOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).y;
+        float bottomSideOfScreenInWorld = Camera.main.ScreenToWorldPoint(new Vector2(0f, 0f)).y;
+
+        if (screenPos.x <= 0 && myRigiBody.velocity.x < 0)
+        {
+            transform.position = new Vector2(rightSideOfScreenInWorld, transform.position.y);
+        }
+        else if(screenPos.x >= Screen.width && myRigiBody.velocity.x > 0)
+        {
+            transform.position = new Vector2(leftSideOfScreenInWorld, transform.position.y);
+        }
+    }
+}
